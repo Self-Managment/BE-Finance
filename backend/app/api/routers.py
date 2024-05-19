4b8dbb4fa4df5caff9c94ec9c996ddac23ae2d93
Арсений Желваков <arsenijzelvakov@MacBook-Pro-Arsenij.local>
2024-05-19 15:17:31 +0300

    удалил инит проекта

--- backend/app/api/routers.py
+++
@@ -1,8 +0,0 @@
-from app.api.endpoints import user, desk
-from fastapi import APIRouter
-from app.api.urls import UserURLS, DeskURLS
-
-router = APIRouter()
-
-router.include_router(user.router, prefix=UserURLS.base_url, tags=["user"])
-router.include_router(desk.router, prefix=DeskURLS.base_url, tags=["desc"])
--- backend/app/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .user import User
-from .desk import Desk, TaskType, Task
